34e0b0f01b693d6b47e2b85cfa5051467f28e6f0
duytu0208 <duytu0208@gmail.com>
2022-05-09 00:56:00 +0700

    [Login][Add: apply keycloak for feature login]

---
+++ matcha/src/main/java/com/latte/matcha/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.latte.matcha.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class LoginController {
+
+    @GetMapping(path = "/list")
+    public List<String> getProducts() {
+        log.info("Login success");
+        return Arrays.asList("Mazda","Toyota","Audi");
+    }
+
+}
