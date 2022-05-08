e5c49ea10e65edec56122c07cb195e9bb8358c9e
duytu0208 <duytu0208@gmail.com>
2022-05-09 01:02:58 +0700

    [Login][Add: apply keycloak for feature login]

--- matcha/src/test/java/com/latte/matcha/MatchaApplicationTests.java
+++ matcha/src/test/java/com/latte/matcha/MatchaApplicationTests.java
@@ -1,13 +1,15 @@
-package com.latte.matcha;
+//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
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MatchaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.latte.matcha;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MatchaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
